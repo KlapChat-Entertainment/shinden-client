1e005ebe6c33baad33059846c965e4e9fc4a0f65
Błażej Drozd <drozdbazej@gmail.com>
2023-03-09 22:38:48 +0100

    Created a separate api to handle electron events

--- src/js/lib/handleWindowMovement.js
+++ src/js/lib/handleWindowMovement.js
@@ -2,11 +2,11 @@ export default function handleWindowMovement() {
     document.onreadystatechange = async () => {
         if(document.readyState == "complete") {
             document.querySelector("#min").addEventListener("click", async () => {
-                await window.shindenAPI.min();
+                await window.electronAPI.min();
             });
             
             document.querySelector("#close").addEventListener("click", async () => {
-                await window.shindenAPI.close();
+                await window.electronAPI.close();
             });
         }
     }
--- src/preload.js
+++ src/preload.js
@@ -3,9 +3,12 @@ const { contextBridge, ipcRenderer } = require("electron");
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
-contextBridge.exposeInMainWorld('shindenAPI', {
+contextBridge.exposeInMainWorld('electronAPI', {
     min: async () => ipcRenderer.send("min"),
     close: async () => ipcRenderer.send("close"),
+});
+
+contextBridge.exposeInMainWorld('shindenAPI', {
     searchAnime: async name => await ipcRenderer.invoke("searchAnime", name),
     getDescription: async linkToSeries => await ipcRenderer.invoke("getDescription", linkToSeries),
     getEpisodes: async linkToSeries => await ipcRenderer.invoke("getEpisodes", linkToSeries),
