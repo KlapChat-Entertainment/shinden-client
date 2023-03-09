d701b43c071b773047a17f1b5b4e8e438415a333
Błażej Drozd <drozdbazej@gmail.com>
2023-03-09 22:33:45 +0100

    Organization of the header file

---
+++ src/api/AnimeSitesHeaders.js
@@ -0,0 +1,26 @@
+module.exports = {
+    Shinden: {
+        FRONTEND: {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "max-age=0",
+            "cookie": "cb-rodo=accepted;",
+            "referer": "https://shinden.pl/",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "same-origin",
+            "sec-fetch-user": "?1",
+            "sec-gpc": "1",
+            "upgrade-insecure-requests": "1",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+        },
+        API: {
+            "Host": "api4.shinden.pl",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0",
+            "Accept": "*/*",
+            "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
+            "Origin": "https://shinden.pl",
+            "Connection": "keep-alive"
+        }
+    }
+}
