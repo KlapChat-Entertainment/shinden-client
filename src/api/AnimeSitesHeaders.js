e8a1bccebf9c0872bbfc742a2d984f257079a757
Błażej Drozd <drozdbazej@gmail.com>
2023-04-06 17:20:12 +0200

    Added headers to handle shinden.pl login

--- src/api/AnimeSitesHeaders.js
+++ src/api/AnimeSitesHeaders.js
@@ -21,6 +21,13 @@ module.exports = {
             "Accept-Language": "pl,en-US;q=0.7,en;q=0.3",
             "Origin": "https://shinden.pl",
             "Connection": "keep-alive"
+        },
+        LOGIN: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.51",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
         }
     }
 }
