283dde798e9bb672b2b186dc4434c6f509c88a14
Błażej Drozd <drozdbazej@gmail.com>
2023-11-28 18:08:26 +0100

    Added basic navigation bar

---
+++ src/lib/navigation/NavBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import NavButton from "./NavButton.svelte";
+</script>
+
+<header data-tauri-drag-region class="flex justify-between items-center w-full h-9 bg-blue-900 select-none cursor-move">
+    <h1 data-tauri-drag-region class="text-white px-4">Shinden Client 4.0</h1>
+    <nav class="h-full">
+        <NavButton buttonType="min" />
+        <NavButton buttonType="close" />      
+    </nav>
+</header>
+
---
+++ src/lib/navigation/NavButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { appWindow } from '@tauri-apps/api/window';
+    
+    type buttonActionType = "min" | "close";
+    
+    export let buttonType: buttonActionType;
+
+    async function buttonAction() {
+        if(buttonType == "min") {
+            await appWindow.minimize();
+        } else if(buttonType == "close") {
+            await appWindow.close();
+        }
+    }
+</script>
+
+<button type="button" class="text-white w-12 h-full text-center text-xl transition-colors hover:bg-blue-700" on:click={buttonAction}>
+    {#if buttonType == "min"}
+        &#95;
+    {:else if buttonType == "close"}
+        &times;
+    {/if}
+</button>
+
