ccb1e9a3830ac8b64a7191d30a3dba4226ddf104
Błażej Drozd <drozdbazej@gmail.com>
2023-11-29 14:49:38 +0100

    Add search results page

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import { loadingState } from "$lib/stores";
+    let animeName = $page.url.searchParams.get('animeName');
+
+    loadingState.set("loading");
+    loadingState.set("failed");
+</script>
+
+<!-- TODO: Table with search results -->
+<h2>{animeName}</h2>
