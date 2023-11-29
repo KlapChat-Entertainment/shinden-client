0c8c211687efdebafe3f283c36990ca9bf2d60ed
Błażej Drozd <drozdbazej@gmail.com>
2023-11-29 12:15:04 +0100

    Add new type od NavButton

--- src/lib/navigation/NavButton.svelte
+++ src/lib/navigation/NavButton.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import { appWindow } from '@tauri-apps/api/window';
     
-    type buttonActionType = "min" | "close";
+    type buttonActionType = "min" | "close" | "link";
     
     export let buttonType: buttonActionType;
+    
+    // Required only when buttonType is link.
+    export let href : string = "";
+    export let value: string = "";
 
     async function buttonAction() {
         if(buttonType == "min") {
@@ -14,11 +18,18 @@
     }
 </script>
 
-<button type="button" class="text-white w-12 h-full text-center text-xl transition-colors hover:bg-blue-700" on:click={buttonAction}>
-    {#if buttonType == "min"}
-        &#95;
-    {:else if buttonType == "close"}
-        &times;
-    {/if}
-</button>
+
+
+{#if buttonType == "min" || buttonType == "close"}
+    <button 
+        type="button" 
+        class="text-white w-12 h-full text-center text-xl transition-colors hover:bg-gray-700" 
+        on:click={buttonAction}>
+        {@html ((buttonType == "min") ? "&#95;" : "&times;") }
+    </button>
+{:else if buttonType == "link"}
+    <a href={href} class="text-white flex items-center w-fit h-full px-4 text-sm transition-colors hover:bg-gray-700">
+        {value}
+    </a>
+{/if}
 
