a13b1d53709c8c06e269bd7c8ca1f18cc181bdde
dekrain <dawidkrainski8@gmail.com>
2023-12-05 17:21:04 +0100

    Use the API on search page

--- src/lib/pages/Search.svelte
+++ src/lib/pages/Search.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+    import { invoke } from '@tauri-apps/api/tauri';
+    import { onMount } from 'svelte';
 	import ResultsTable from "$lib/searchResult/resultsTable.svelte";
     import { loadingState } from "$lib/stores";
 	import { LoadingState } from "$lib/types";
     import { animeName } from "$lib/stores";
     $loadingState = LoadingState.LOADING;
-    
-    // TODO: anime search
 
-    $loadingState = LoadingState.SUCCESS;
+    onMount(async () => {
+        try {
+            const anime = await invoke('search_anime', { 'anime': $animeName });
+            console.log(anime);
+            $loadingState = LoadingState.SUCCESS;
+        } catch(err) {
+            console.error('Oh noes!', err);
+            $loadingState = LoadingState.FAILED;
+        }
+    });
 
 </script>
 
