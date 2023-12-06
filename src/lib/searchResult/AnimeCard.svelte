ba2c1bd1d1c1204f27a44ab7031ec0a569d4340d
Błażej Drozd <drozdbazej@gmail.com>
2023-12-06 17:25:26 +0100

    Search results UI update

---
+++ src/lib/searchResult/AnimeCard.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { selectedAnimeId } from "$lib/stores";
+	import { AppState, type AnimeSearchResult } from "$lib/types";
+    export let anime: AnimeSearchResult;
+
+    async function handleClick() {
+        $selectedAnimeId = anime.online_id;
+        await goto(AppState.ANIME);
+    }
+</script>
+
+<button type="button" class="flex bg-gray-700 p-3 rounded-xl shadow-sm shadow-black items-stretch" on:click={handleClick}>
+    <div>
+        <img src={anime.image_link} alt={anime.name} class="w-28 rounded-xl shadow-md shadow-black">
+    </div>
+    <div class="flex flex-1 flex-col text-center">
+        <h3 class="text-white text-center text-xl border-b mx-5 py-1">{anime.name}</h3>
+        <div class="grid grid-cols-3 text-white flex-1 items-center">
+            <div class="text-xl font-bold">Ocena</div>
+            <div class="text-xl font-bold">Odcinki</div>
+            <div class="text-xl font-bold">Typ</div>
+            <div class="text-2xl">{anime.rating.toFixed(2)} / 10</div>
+            <div class="text-2xl">{anime.episode_count}</div>
+            <div class="text-2xl">{anime.kind}</div>
+        </div>
+    </div>
+</button>
--- src/lib/searchResult/resultsTable.svelte
+++ src/lib/searchResult/resultsTable.svelte
@@ -1,29 +1,11 @@
 <script lang="ts">
     import type { AnimeSearchResult } from "$lib/types";
-	import AnimeGotoButton from "./AnimeGotoButton.svelte";
+	import AnimeCard from "./AnimeCard.svelte";
     export let searchResults: Array<AnimeSearchResult>;
 </script>
 
-<table class="w-full border-black border">
-    <tr class="text-white">
-        <th class="border-black border">Okładka</th>
-        <th class="border-black border">Nazwa</th>
-        <!-- We may have to limit the name of the anime so that it doesn't ruin the interface when it gets too long. -->
-        <th class="border-black border">Typ</th>
-        <th class="border-black border">Epizody</th>
-        <th class="border-black border">Ocena</th>
-    </tr>
+<div class="w-full flex flex-col gap-5">
     {#each searchResults as anime}
-        <tr class="text-center text-white border-black border">
-            <td class="text-center border-black border p-4">
-                <img class="w-28 mx-auto shadow-lg shadow-black" src={anime.image_link} alt="{anime.name}">
-            </td>
-            <td class="border-black border">
-                <AnimeGotoButton name={anime.name} id={anime.online_id} />
-            </td>
-            <td class="border-black border">{anime.kind}</td>
-            <td class="border-black border">{anime.episode_count}</td>
-            <td class="border-black border">{anime.rating.toFixed(2)}</td>
-        </tr>
+        <AnimeCard anime={anime} />
     {/each}
-</table>
+</div>
