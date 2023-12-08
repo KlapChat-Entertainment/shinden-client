dd06d43aecbfd57e7223e852777d36decabfaf8c
Błażej Drozd <drozdbazej@gmail.com>
2023-12-08 09:10:47 +0100

    Fix size of LoadingAnimation

--- src/lib/LoadingAnimation.svelte
+++ src/lib/LoadingAnimation.svelte
@@ -20,9 +20,9 @@
     }
 </script>
 
-<div class="w-9 h-9 flex gap-2 items-center font-bold text-lg">
+<div class="h-9 flex gap-2 items-center font-bold text-lg">
     {#if $loadingState == LoadingState.LOADING}
-        <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        <svg class="w-9" version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
             <path fill="#fff" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
             <animateTransform 
