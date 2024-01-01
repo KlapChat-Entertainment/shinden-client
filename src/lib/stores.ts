3107ae957b927b713a9327f214fd1d1e9c04a018
Błażej Drozd <drozdbazej@gmail.com>
2024-01-01 17:20:24 +0100

    Add selectedEpisode store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,8 @@
 import { writable } from "svelte/store";
 import { LoadingState } from "./types";
+import type { EpisodeInfo } from "./types";
 
 export const loadingState = writable<LoadingState>(LoadingState.SUCCESS);
 export const animeName = writable<string>("");
 export const selectedAnimeId = writable<number>(0);
+export const selectedEpisode = writable<EpisodeInfo>();
