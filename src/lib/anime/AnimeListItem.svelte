1ef17bc6671cb256a343102d4150cc7f0fc957b8
Błażej Drozd <drozdbazej@gmail.com>
2024-01-01 17:39:25 +0100

    Prepare to create players view

--- src/lib/anime/AnimeListItem.svelte
+++ src/lib/anime/AnimeListItem.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
 	import { selectedEpisode } from "$lib/stores";
-    import type { EpisodeInfo } from "$lib/types";
+    import { AppState, type EpisodeInfo } from "$lib/types";
     export let episode: EpisodeInfo;
 
     async function handleClick() {
         $selectedEpisode = episode;
+        await goto(AppState.PLAYERS);
     }
 </script>
 
 <li>
     <button on:click={handleClick} class="bg-gray-500 text-left rounded-xl px-3 py-1 my-2 shadow-sm shadow-black w-full">
-        {episode.index}. {episode.name}
+        {episode.index}. {episode.name ? episode.name : "Brak nazwy odcinka"}
     </button>
 </li>
--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -1,1 +1,17 @@
+<script lang="ts">
+	import { loadingState, selectedEpisode } from "$lib/stores";
+	import { LoadingState } from "$lib/types";
+    
+    $loadingState = LoadingState.LOADING;
+
+    $loadingState = LoadingState.SUCCESS;
+
+</script>
+
 <!-- TODO: Players view -->
+
+<div class="text-white">
+    <p>Selected anime episode index: {$selectedEpisode.index}</p>
+    <p>Selected anime episode name: {$selectedEpisode.name}</p>
+    <p>Selected anime episode link: {$selectedEpisode.link}</p>
+</div>
