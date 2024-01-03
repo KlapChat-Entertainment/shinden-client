34cb225364f5c9983dca25ed7bc1f0946eca29f5
Błażej Drozd <drozdbazej@gmail.com>
2024-01-03 13:14:14 +0100

    Fix scrollbar on AnimeDescription

--- src/lib/anime/AnimeDescription.svelte
+++ src/lib/anime/AnimeDescription.svelte
@@ -4,6 +4,8 @@
     export let animeDetails: AnimeDetails;
 </script>
 
-<div class="h-full overflow-y-auto bg-gray-600 shadow-sm shadow-black rounded-xl">
-    <p class="text-justify text-white px-3 py-3">{animeDetails.description}</p>
+<div class="h-full py-3 bg-gray-600 shadow-sm shadow-black rounded-xl">
+    <div class="overflow-y-auto h-full px-3">
+        <p class="text-justify text-white">{animeDetails.description}</p>
+    </div>
 </div>
