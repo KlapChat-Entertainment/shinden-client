851b3aa8f496fa681226a3dc5f6c5cd28adba4bf
Błażej Drozd <drozdbazej@gmail.com>
2024-03-18 21:04:53 +0100

    Improved login screen

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -8,6 +8,7 @@ handleWindowMovement();
 const FORM = document.querySelector("form");
 const LOGIN_STATUS_HTML = document.querySelector("#loginStatus");
 const LOGOUTBUTTON: HTMLButtonElement = document.querySelector("#clearCookies");
+const USERIMAGE: HTMLImageElement = document.querySelector(".userImage");
 
 FORM.addEventListener("submit", async event=>{
     event.preventDefault();
@@ -24,23 +25,39 @@ FORM.addEventListener("submit", async event=>{
 });
 
 async function updateLoginInfo() {
-    FORM.style.display="none";
     const LOGIN_STATUS: boolean = await window.shindenAPI.getLoginStatus();
+    const EMAIL_IN: HTMLInputElement = document.querySelector("#email");
+    const PASSWORD_IN: HTMLInputElement = document.querySelector("#password");
+    const LOGIN_BUTTON: HTMLButtonElement = document.querySelector("#login");
 
     if(LOGIN_STATUS) {
         const USERNAME = await window.shindenAPI.getUserName();
+        const USER_IMAGE = await window.shindenAPI.getUserProfileImage();
+
+        if(USERIMAGE) {
+            USERIMAGE.src = USER_IMAGE;
+        }
+
         if(USERNAME) {
             LOGIN_STATUS_HTML.innerHTML = `Zalogowano jako ${USERNAME}`;
         } else {
             LOGIN_STATUS_HTML.innerHTML = `Zalogowano, brak dostępu do nazwy użytkownika!`;
         }
 
-        LOGOUTBUTTON.style.display="block";
+        LOGOUTBUTTON.disabled = false;
+        EMAIL_IN.disabled = true;
+        EMAIL_IN.value = "";
+        PASSWORD_IN.disabled = true;
+        PASSWORD_IN.value = "";
+        LOGIN_BUTTON.disabled = true;
         
     } else {
-        FORM.style.display="flex";
+        EMAIL_IN.disabled = false;
+        PASSWORD_IN.disabled = false;
+        LOGIN_BUTTON.disabled = false;
+        LOGOUTBUTTON.disabled = true;
         LOGIN_STATUS_HTML.innerHTML = "Brak danych logowania w pamięci.";
-        LOGOUTBUTTON.style.display="none";
+        USERIMAGE.src="";
     }
 
     finishLoading();
